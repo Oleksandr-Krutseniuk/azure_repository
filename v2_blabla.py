2c8b58438b6aa3482a95dbddd440d417a27366c6
raccoon <a.krutseniuk@gmail.com>
2023-06-23 18:47:57 +0300

    python-dns-extraction v2

--- v2_blabla.py
+++ v2_blabla.py
@@ -1,45 +1,55 @@
+import json
 import requests
-import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
-def get_dns_records_from_cloudflare(zone_id, api_token):
-    url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records"
+def get_dns_records_from_cloudflare():
+    vars_file_path = '/home/sashaa/python/vars.json'
+
+    with open(vars_file_path) as config_file:
+        config = json.load(config_file)
+    CLOUDFLARE_TOKEN = config['CLOUDFLARE_TOKEN']
+    ZONE_ID = config['ZONE_ID']
+
     headers = {
-        "Authorization": f"Bearer {api_token}",
-        "Content-Type": "application/json"
+        'Authorization': f'Bearer {CLOUDFLARE_TOKEN}',
+        'Content-Type': 'application/json'
     }
-    response = requests.get(url, headers=headers)
+
+    response = requests.get(f'https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records', headers=headers)
+
     return response.json()['result']
 
-def write_records_to_sheet(records, domain, spreadsheet):
-    # Если лист с именем домена уже существует, то выберем его, иначе создадим новый
-    try:
-        worksheet = spreadsheet.worksheet(domain)
-    except gspread.exceptions.WorksheetNotFound:
-        worksheet = spreadsheet.add_worksheet(title=domain, rows="100", cols="20")
-        
-    # Предполагается, что все записи имеют одинаковые поля, поэтому мы используем поля первой записи в качестве заголовков
-    headers = list(records[0].keys())
-    worksheet.append_row(headers)
-    for record in records:
-        row = [record[header] for header in headers]
-        worksheet.append_row(row)
+def write_records_to_sheet(dns_records):
+    vars_file_path = '/home/sashaa/python/vars.json'
 
-def main():
-    api_token = "your_cloudflare_api_token"
-    google_keyfile = "your_service_account_key.json"
+    with open(vars_file_path) as config_file:
+        config = json.load(config_file)
+    google_keyfile = config['google_keyfile']
+    sheet_name = config['sheet_name']
 
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(google_keyfile, scope)
     gc = gspread.authorize(credentials)
-    
-    spreadsheet = gc.open("DNS Records")
-    domains = ["example1.com", "example2.com"]
 
-    for domain in domains:
-        zone_id = ...  # Здесь нужно получить ID зоны для домена
-        dns_records = get_dns_records_from_cloudflare(zone_id, api_token)
-        write_records_to_sheet(dns_records, domain, spreadsheet)
+    spreadsheet = gc.open(sheet_name)
+
+    domain = dns_records[0]['name']
+
+    try:
+        worksheet = spreadsheet.worksheet(domain)
+    except gspread.WorksheetNotFound:
+        worksheet = spreadsheet.add_worksheet(title=domain, rows="100", cols="5")
+
+    worksheet.clear()
+
+    for record in dns_records:
+        row = [record['name'], record['type'], record['content'], record['ttl'], record.get('comment', '')]
+        worksheet.append_row(row)
+
+def main():
+    dns_records = get_dns_records_from_cloudflare()
+    write_records_to_sheet(dns_records)
 
 if __name__ == "__main__":
     main()
